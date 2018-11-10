c682b2d6eaeae149b2f12af1b04e63b7ec8bdd0c
Kasidit Ratanawongpitak <kasidit_128@hotmail.com>
2018-11-10 17:04:21 +0700

    ..

---
+++ core/src/com/dnfb/game/Main.java
@@ -0,0 +1,38 @@
+package com.dnfb.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import manage.GSM;
+import state.PlayingState;
+
+public class Main extends ApplicationAdapter {
+	public static final int WIDTH = 480;
+	public static final int HEIGHT = 800;
+	public static final String TITLE = "Definitely Not Flappy Bird";
+	private GSM gsm;
+	SpriteBatch batch;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		gsm = new GSM();
+		gsm.push(new PlayingState(gsm));
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(batch);
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		gsm.dispose();
+	}
+}
